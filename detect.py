4ae53690f365fdeb5da9827342f47c9c42f247e2
Miranda Fuentes Johann <jjmirandaf@gmail.com>
2023-02-07 11:20:41 +0100

    Add diagonal to txt output.

--- detect.py
+++ detect.py
@@ -34,6 +34,7 @@ import platform
 import sys
 from pathlib import Path
 
+import numpy as np
 import torch
 
 FILE = Path(__file__).resolve()
@@ -163,7 +164,9 @@ def run(
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        #line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        diagonal = np.sqrt(xywh[2] * xywh[2] + xywh[3] * xywh[3])
+                        line = (*xywh, diagonal, conf) if save_conf else (*xywh, diagonal)  # label format
                         with open(f'{txt_path}.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
